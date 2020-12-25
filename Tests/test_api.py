22eadb1a773963161faa6d2efcf4d481dfdcbc36
Ayman Elsayeed <ayman.elsayeed@mdclone.com>
2020-12-25 16:35:09 +0200

    add parametrize unit tests

--- Tests/test_api.py
+++ Tests/test_api.py
@@ -3,6 +3,8 @@ from pytest import mark
 
 class TestCasesApi:
     """Basics test for the API"""
+    even_numbers = [(i, {'message': 'the number is even', 'number': i}) for i in range(0, 10, 2)]
+    odd_numbers = [(i, f"http://localhost/is_odd/{i}") for i in range(1, 10, 2)]
 
     def test_get_api(self, client):
         """test availability of the api"""
@@ -14,6 +16,12 @@ class TestCasesApi:
         assert response.status_code == 200
         assert response.get_json() == {'message': 'the number is even', 'number': 2}
 
+    @mark.parametrize(["number", "output"], even_numbers)
+    def test_even_list(self, client, number, output):
+        response = client.get(f"/{number}")
+        assert response.status_code == 200
+        assert response.get_json() == output
+
     def test_odd(self, client):
         n = 3
         response = client.get(f"/{n}")
@@ -21,3 +29,9 @@ class TestCasesApi:
         # print(dir(response))
         # print(response.location)
         assert response.location == f"http://localhost/is_odd/{n}"
+
+    @mark.parametrize(["number", "output"], odd_numbers)
+    def test_odd_list(self, client, number, output):
+        response = client.get(f"/{number}")
+        assert response.status_code == 302
+        assert response.location == output
