4cb278b27b89057035a8d670ef36f7bcd930ef9d
Ayman Elsayeed <ayman.elsayeed@mdclone.com>
2020-12-25 15:53:31 +0200

    add unit tests

--- Tests/test_api.py
+++ Tests/test_api.py
@@ -1,7 +1,6 @@
 from pytest import mark
 
 
-@mark.smoke
 class TestCasesApi:
     """Basics test for the API"""
 
@@ -10,3 +9,16 @@ class TestCasesApi:
         client = get_api_client
         print("client: ", client.get("/"))
         assert client.get("/").status_code == 200
+
+    def test_even(self, get_api_client):
+        response = get_api_client.get("/2")
+        assert response.status_code == 200
+        assert response.get_json() == {'message': 'the number is even', 'number': 2}
+
+    def test_odd(self, get_api_client):
+        n = 3
+        response = get_api_client.get(f"/{n}")
+        assert response.status_code == 302
+        # print(dir(response))
+        # print(response.location)
+        assert response.location == f"http://localhost/is_odd/{n}"
