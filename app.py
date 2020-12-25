53af439d20de7b211566a421873cc5f92ebcfd43
Ayman Elsayeed <ayman.elsayeed@mdclone.com>
2020-12-25 13:01:21 +0200

    add functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,28 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/<int:code>')
+def is_even(code):
+    if code % 2 == 0:
+        return jsonify(number=code, message="the number is even")
+    else:
+        return redirect(f"/is_odd/{code}")
+        # abort(404)
+
+
+@app.route('/is_odd/<int:n>')
+def is_odd(n):
+    if n % 2 != 0:
+        return jsonify(number=n, message="the number is odd")
+    else:
+        abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page_not_found.html"), 404
 
 
 if __name__ == '__main__':
