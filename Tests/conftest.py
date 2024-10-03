7adad885a12be04bbdcdedd59cf3c61572b05148
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-10-03 14:37:29 +0300

    add docstring.

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -9,16 +9,31 @@ def pytest_addoption(parser):
 
 @fixture(scope="session")
 def env(request):
+    """
+    get the environment to run the tests against.
+    :param request: the request object
+    :return: the environment to run the tests against
+    """
     env = request.config.getoption("--env")
     return env
 
 
 def get_config_class(env):
+    """
+    get the config class based on the environment.
+    :param env:  the environment to run the tests against
+    :return: the config class
+    """
     return FactoryConfigClass(env).config
 
 
 @fixture(scope="session")
 def client(env):
+    """
+    create a test client to run tests against.
+    :param env: the environment to run the tests against
+    :return: an instance of the test client
+    """
     run_env = get_config_class(env=env)
     app.app.config.update(run_env.__dict__)
     api = app.app.test_client()
