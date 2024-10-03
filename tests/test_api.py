7452713b994bf9803f79079d086e455313e388ec
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-10-03 14:52:05 +0300

    fix tests.

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,7 +10,7 @@ from pytest import mark
 class TestCasesApi:
     """Basics test for the API"""
     even_numbers = [(i, {'message': 'the number is even', 'number': i}) for i in range(0, 10, 2)]
-    odd_numbers = [(i, f"http://localhost/is_odd/{i}") for i in range(1, 10, 2)]
+    odd_numbers = [(i, f"/is_odd/{i}") for i in range(1, 10, 2)]
 
     def test_get_api(self, client):
         """test availability of the api"""
@@ -34,7 +34,7 @@ class TestCasesApi:
         assert response.status_code == 302
         # print(dir(response))
         # print(response.location)
-        assert response.location == f"http://localhost/is_odd/{n}"
+        assert response.location == f"/is_odd/{n}"
 
     @mark.parametrize(["number", "output"], odd_numbers)
     def test_odd_list(self, client, number, output):
